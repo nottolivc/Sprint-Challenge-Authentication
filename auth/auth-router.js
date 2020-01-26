cb49c48615b0b69b9c7e177c9b21f3fccc2818d8
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-01-25 17:14:30 -0800

    adding routes, middleware token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const secret = require('../auth/authenticate').jwtToken;
+const jwt = require('jsonwebtoken');
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+const { authenticate } = require('../auth/authenticate-middleware.js');
 
-module.exports = router;
+module.exports = server => {
+  server.post('/api/register', register);
+  server.post('/api/login', login);
+  server.get('/api/jokes', authenticate, getJokes);
+};
+
+function generateToken(user) {
+  const payload = {
+    subject: user
+  }
+
+  const options = {
+    expiresIn: '7d'
+  }
+
+  return jwt.sign(payload, secret, options)
+};
+
+function register(req, res) {
+  const user = req.body
+  const token = generateToken(user.username);
+  if (user.username && user.password) {
+    user.password = bcrypt.hashSync(user.password, 12);
+    db('users').insert(user)
+    .then(response => {
+      res.status(200).json({user: user.username, token})
+    })
+  } else {
+    res.status(400).json({message: 'username and password not provided.'})
+  }
+}
+
+function login(req, res) {
+  const { username, password } = req.body
+  if (username && password) {
+    db('users').where({username}).first()
+    .then(response => {
+      const user = response
+      if (user) {
+        const token = generateToken(response.username)
+        res.status(200).json({token: token})
+      } 
+    })
+    .catch(err => req.status(500).json({message:'error logging in!'}))
+  } else {
+    res.status(400).json({message: 'invalid credentials!'})
+  }
+}
+
+
+
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+
+const jwtToken = process.env.JWT_SECRET || 'super secret thing here';
+
+
+// auth middleware function
+function authenticate(req, res, next) {
+  const token = req.get('Authorization');
+
+  if (token) {
+    jwt.verify(token, jwtKey, (error, decoded) => {
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    })
+  } else {
+    return res.status(401).json({
+      error: 'you shall not pass!',
+    })
+  }
+}
+
+module.exports = {
+  authenticate,
+  jwtToken
+};
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+// module.exports = (req, res, next) => {
+//   res.status(401).json({ you: 'shall not pass!' });
+// };
+
